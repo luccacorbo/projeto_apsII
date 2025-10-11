10501572c9e8b9c0695a4a7afccd9b3f724a1392
Lucca Corbo <lucca.ccorbo@gmail.com>
2025-10-11 19:56:26 -0300

    acesso de usuario criado

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from routes.auth import auth
+from routes.workspaces import work
 
 app = Flask(__name__)
 app.secret_key = "chave_secreta"
 
 # registra o blueprint de autenticação
 app.register_blueprint(auth)
+app.register_blueprint(work)
 
 if __name__ == "__main__":
     app.run(debug=True)
